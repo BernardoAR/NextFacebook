2f3ceb940ba55bae2054a70ee18a3e0e29a365ee
Bernardo Alves Roballo <bernardo_roballo@hotmail.com>
2021-07-06 21:03:02 -0300

    Added the hide of scroll, and the widgets from the right side.

--- components/Feed.js
+++ components/Feed.js
@@ -3,7 +3,7 @@ import Stories from './Stories';
 import Posts from './Posts';
 function Feed() {
   return (
-    <div className='flex flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto'>
+    <div className='flex flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide'>
       {/* Stories */}
       <div className='mx-auto max-w-md md:max-w-lg lg:max-w-2xl'>
         <Stories />
